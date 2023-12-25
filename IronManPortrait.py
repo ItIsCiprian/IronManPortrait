70c5b863b50e0b70c3b00e26c0c0509db4db1581
Ionut Ciprian Anescu <ionutcipriananescu@gmail.com>
2023-12-25 22:30:11 +0200

    Update IronManPortrait.py

--- IronManPortrait.py
+++ IronManPortrait.py
@@ -1,38 +1,45 @@
-import turtle as t
+import turtle as t  # Import the turtle module and give it an alias 't'
 
-# Set up the screen
+# Set up the drawing window with specific dimensions and background color
 t.setup(800, 600)
 t.bgcolor("black")
-t.title("Iron Man Face")
+t.title("Iron Man Face")  # Title for the window, which will display at the top
 
-# Create a turtle for drawing
+# Initialize a new turtle instance for drawing shapes
 pen = t.Turtle()
-pen.speed(0)
-pen.color("red")
-pen.pensize(2)
+pen.speed(0)  # Set the drawing speed to the fastest (0 means no animation)
+pen.color("red")  # Initial drawing color for the turtle's pen
+pen.pensize(2)  # Set the width of the pen to 2 pixels
 
-# Draw a circle with a given color, radius, and position
+# Define a function to draw a filled circle with specified parameters
 def draw_circle(color, radius, x, y):
-    pen.penup()
-    pen.fillcolor(color)
-    pen.goto(x, y - radius)
-    pen.pendown()
-    pen.begin_fill()
-    pen.circle(radius)
-    pen.end_fill()
+    pen.penup()  # Lift the pen to move it without drawing
+    pen.fillcolor(color)  # Set the fill color for the next shape
+    pen.goto(x, y - radius)  # Move the pen to the starting position of the circle
+    pen.pendown()  # Put the pen down to start drawing
+    pen.begin_fill()  # Start filling the shape with the set color
+    pen.circle(radius)  # Draw the circle with the given radius
+    pen.end_fill()  # Complete the filling process for the circle
 
-# Draw Iron Man's face
+# Use the draw_circle function to create the components of Iron Man's face
+# The parameters specify the color, radius, and central position of each circle
+
+# Main face outline
 draw_circle("red", 100, 0, 0)
+# Eyes
 draw_circle("white", 60, -40, 60)
 draw_circle("white", 60, 40, 60)
+# Eye reflections
 draw_circle("white", 25, -37, 75)
 draw_circle("white", 25, 37, 75)
+# Arc Reactor and details
 draw_circle("blue", 15, 0, -35)
 draw_circle("blue", 5, -15, -35)
 draw_circle("blue", 5, 15, -35)
 
-# Hide the turtle
+# Hide the drawing turtle to give a clean final image
 pen.hideturtle()
 
-# Keep the window open until it's closed by the user
+# Start the event loop for the window to listen for events, like mouse clicks or key presses
+# This is necessary to keep the window open; otherwise, it would close immediately after drawing
 t.mainloop()
